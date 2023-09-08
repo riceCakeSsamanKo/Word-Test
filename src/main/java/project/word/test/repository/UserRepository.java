2c35cbae5413ea93ffa372b23cdd0e290fa3ba51
Min Jeon <mulsoap0504@gmail.com>
2023-09-08 19:43:40 +0900

    Create UserRepository.java

---
+++ src/main/java/project/word/test/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package project.word.test.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private final EntityManager em;
+
+}
