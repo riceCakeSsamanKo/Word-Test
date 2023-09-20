94f1bf110a528a7a04855dff7b3c18de1ef2c107
Min Jeon <mulsoap0504@gmail.com>
2023-09-20 13:02:10 +0900

    update

--- src/main/java/project/word/test/controller/UserContoller.java
+++ src/main/java/project/word/test/controller/UserContoller.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import project.word.test.domain.AccountType;
 import project.word.test.domain.Group;
 import project.word.test.domain.LogInInformation;
 import project.word.test.domain.User;
@@ -13,7 +15,10 @@ import project.word.test.dto.UserForm;
 import project.word.test.service.GroupService;
 import project.word.test.service.UserService;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
+
+import static project.word.test.domain.AccountType.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,13 +46,42 @@ public class UserContoller {
     }
 
     @GetMapping("/login")
-    public String login() {
-
-        return null;
+    public String login(Model model) {
+        model.addAttribute("form", new UserForm());
+        return "loginPage";
     }
 
-    @GetMapping("/user")
-    public String userInfo() {
-        return null;
+    @PostMapping("/login")
+    public String login(Model model, UserForm userForm, HttpSession session) {
+        User loggedInUser = userService.findUser(userForm.getId());
+        if (loggedInUser != null) {
+            session.setAttribute("loggedInUser",loggedInUser);
+            model.addAttribute("loggedIn", true);
+            if(loggedInUser.getAccountType() == ADMIN){
+                model.addAttribute("isAdmin", true);
+            }
+            return "home";
+        } else{
+            // 없는 아이디이거나 비밀번호입니다 경고 발생
+            model.addAttribute("noMemberExist", true);
+            return "loginPage";
+        }
+    }
+
+    @RequestMapping("/user")
+    public String userInfo(HttpSession session,Model model) {
+        User loggedInUser = (User) session.getAttribute("loggedInUser");
+        model.addAttribute("user", loggedInUser);
+
+        return "userInfo";
+    }
+
+    @GetMapping("/user/edit")
+    public String editUserInfo(HttpSession session, Model model) {
+        User loggedInUser = (User) session.getAttribute("loggedInUser");
+        model.addAttribute("user", loggedInUser);
+        model.addAttribute("form", new UserForm());
+
+        return "editUserInfo";
     }
 }
